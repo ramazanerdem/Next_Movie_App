ec4abc387df973f80e044271486783f3d2cc41f9
Ramazan Erdem <ramazan.erdem.du@gmail.com>
2023-06-15 19:27:20 +0300

    fixed ascii

--- components/header/index.js
+++ components/header/index.js
@@ -7,14 +7,14 @@ const Header = () => {
       <div className="px-10 relative flex justify-between items-center">
         <Link
           className="flex items-center gap-2 font-bold text-yellow-600 text-2xl xl:text-4xl"
-          href="/"
+          href="&#47;"
         >
           <RiMovie2Fill /> ZORTFLIX
         </Link>
         <nav className="hidden md:flex items-center md:text-xl gap-10 font-semibold">
-          <Link href="/">Movies</Link>
-          <Link href="/">Series</Link>
-          <Link href="/">Kids</Link>
+          <Link href="&#47;">Movies</Link>
+          <Link href="&#47;">Series</Link>
+          <Link href="&#47;">Kids</Link>
         </nav>
       </div>
     </header>
